f6cbaa42f57d736647e9f413f88243e9a40eef2f
Shubham Purohit <shubhampurohit24@hotmail.com>
2019-04-27 23:25:25 +0530

    model created
    Sequential model created and various layers added:
     -flatten layer to flatten the data
    -two dense layers with relu activation and 128  neurons
    -final softmax layer to make predictions

--- first.py
+++ first.py
@@ -14,3 +14,11 @@ x_test = tf.keras.utils.normalize(x_test, axis=1, order=1)
 
 plt.imshow(x_train[0], plt.cm.binary)
 plt.show()
+
+model = tf.keras.Sequential([
+           tf.keras.layers.Flatten(),
+           tf.keras.layers.Dense(128,activation=tf.nn.relu),
+           tf.keras.layers.Dense(128,activation=tf.nn.relu),
+           tf.keras.layers.Dense(10 ,activation=tf.nn.softmax)
+        ])
+
