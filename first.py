247bebe97e1ae0fc77e4e23c8d56018a42cb9200
Shubham Purohit <shubhampurohit24@hotmail.com>
2019-04-27 23:21:49 +0530

    Normalize the image data
    normalize the image data as the values range from 0 to 255

--- first.py
+++ first.py
@@ -4,3 +4,13 @@ import matplotlib.pyplot as plt
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+plt.imshow(x_train[0], plt.cm.binary)
+plt.show()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1, order=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1, order=1)
+
+plt.imshow(x_train[0], plt.cm.binary)
+plt.show()
